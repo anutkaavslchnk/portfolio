866bb087fa5396274a753f7f306611d2959f6892
Anna Vasylchenko <anutochkaaaaa14@gmail.com>
2024-12-13 23:26:54 +0100

    fixed

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -162,6 +162,7 @@ const AboutMe = () => {
         </div>
        
       </section>
+
     </main>
   );
 };
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,7 +1,11 @@
 import s from './Projects.module.css';
 console.log(s);
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div>
+    <h2 className={s.title}>My projects</h2>
+    </div>
+  );
 };
 
 export default Projects;
