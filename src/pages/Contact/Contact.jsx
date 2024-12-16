5dc750faa74f2c5884adbb1099bf22faa59f2012
Anna Vasylchenko <anutochkaaaaa14@gmail.com>
2024-12-16 22:55:15 +0100

    fixed the projects page and added the contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,26 @@
 
+import { FaLinkedin, FaTelegramPlane } from 'react-icons/fa';
 import s from './Contact.module.css';
 console.log(s);
+import { IoLogoGithub } from "react-icons/io";
 
+import { BiLogoGmail } from 'react-icons/bi';
+import { BsFillTelephoneFill } from 'react-icons/bs';
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+  <>
+  <p className={s.order}>Here you can order the website</p>
+  <p className={s.contacts}>Here is my contacts:</p>
+  <ul className={s.list}>
+    <li><a href=""><IoLogoGithub fill="black" size="40px" /></a></li>
+    <li><a href=""><FaLinkedin fill="black" size="40px" /></a></li>
+    <li><a href=""><FaTelegramPlane fill="black" size="40px" /></a></li>
+    <li><a href=""><BiLogoGmail fill="black" size="40px" /></a></li>
+    <li><a href=""><BsFillTelephoneFill fill="black" size="30px" /></a></li>
+  </ul>
+  
+  </>
+  )
 };
 
 export default Contact;
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -4,91 +4,107 @@ import img2 from '/public/fb.png';
 import img3 from '/public/js.png';
 import img4 from '/public/fr.png';
 import img5 from '/public/st.png';
-console.log(s);
+import { useState } from 'react';
+
 const Projects = () => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage] = useState(3);
+
+
+  const projects = [
+    {
+      id: 1,
+      img: img1,
+      title: 'Task Pro',
+      year: '2024',
+      type: 'Web app',
+      description: 'A team project. An app that will help you plan your day. This is a full-stack application. I wrote the code for the backend part of authorization and authentication',
+      skills: '·React.js ·Node.js ·MongoDB ·JavaScript ·HTML6 ·CSS ·Vite ·Render ·Redux ·REST API',
+    },
+    {
+      id: 2,
+      img: img2,
+      title: 'Phone book',
+      year: '2024',
+      type: 'Web app',
+      description: 'In this web application, you will have the opportunity to create your own account in the contact book, where you can add new contacts, delete them, and search by number and name. Individual project',
+      skills: '·HTML5 ·CSS3 ·JavaScript ·React.js ·Node.js ·Redux ·Axios ·Vite',
+    },
+    {
+      id: 3,
+      img: img3,
+      title: 'Your energy',
+      year: '2023',
+      type: 'Web app',
+      description: 'Team project. This site will help you create your own workout program, has a variety of workouts for each body part and an explanation of how it works. I was responsible for the footer of the site and all its functionality',
+      skills: '·HTML5 ·CSS3 ·JavaScript ·Vite ·Communication ·Teamwork',
+    },
+    {
+      id: 4,
+      img: img4,
+      title: 'Veggieboost',
+      year: '2023',
+      type: 'Web app',
+      description: 'Team project. A website that helps you easily shop for fresh vegetables from home. It is adapted for all devices, has a modal window and several animations. I developed the “organic vegetables” section, which is adapted to all devices',
+      skills: '·HTML5 ·CSS3 ·JavaScript ·Vite ·Communication ·Teamwork',
+    },
+    {
+      id: 5,
+      img: img5,
+      title: 'Web studio',
+      year: '2023',
+      type: 'Web app',
+      description: 'The website of the web studio is adapted for all types of devices and includes modal windows, a responsive design, effects and transitions.',
+      skills: '·HTML5 ·CSS3 ·JavaScript',
+    },
+  ];
+
+
+  const indexOfLastProject = currentPage * perPage;
+  const indexOfFirstProject = indexOfLastProject - perPage;
+
+
+  const currentProjects = projects.slice(indexOfFirstProject, indexOfLastProject);
+
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <section className={s.cont}>
-    <h2 className={s.title}>My projects</h2>
+      <p className={s.title}>My projects</p>
 
-    <ul className={s.list}>
+      <ul className={s.list}>
+        {currentProjects.map((project) => (
+          <li key={project.id} className={s.items}>
+            <img src={project.img} alt={project.title} className={s.img} />
+            <div>
+              <p className={s.par_f}>{project.title}</p>
+              <div className={s.cont_txt}>
+                <div className={s.year}>{project.year}</div>
+                <p className={s.type}>{project.type}</p>
+              </div>
+              <p className={s.description}>{project.description}</p>
+              <div className={s.line}></div>
+              <p className={s.skills}>Skills</p>
+              <p className={s.listskills}>{project.skills}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
 
-      <li className={s.items}>
-        <img src={img1} alt="task pro" className={s.img} />
-        <div>
-          <p className={s.par_f}>Task Pro</p>
-          <div className={s.cont_txt}>
-          <div className={s.year}>2024</div>
-          <p className={s.type}>Web app</p>
-          </div>
-          <p className={s.description}>A team project. An app that will help you plan your day. This is a full-stack application. I wrote the code for the backend part of authorization and authentication</p>
-          <div className={s.line}></div>
-          <p className={s.skills}>Skills</p>
-          <p className={s.listskills}>React.js · Node.js · MongoDB · JavaScript · HTML6 · CSS · Vite · Render · Redux · REST API</p>
-        </div>
-       
-      </li>
-      <li className={s.items}>
-      <img src={img2} alt="phone book" className={s.img} />
-      <div>
-          <p className={s.par_f}>Phone book</p>
-          <div className={s.cont_txt}>
-          <div className={s.year}>2024</div>
-          <p className={s.type}>Web app</p>
-          </div>
-          <p className={s.description}>In this web application, you will have the opportunity to create your own account in the contact book, where you can add new contacts, delete them, and search by number and name. Individual project</p>
-          <div className={s.line}></div>
-          <p className={s.skills}>Skills</p>
-          <p className={s.listskills}>HTML5 · CSS3 · JavaScript · React.js · Node.js · Redux · Axios · Vite</p>
-        </div>
 
-       
-      </li>
-      <li className={s.items}>
-      <img src={img3} alt="your energy" className={s.img} />
-      <div>
-          <p className={s.par_f}>Your energy</p>
-          <div className={s.cont_txt}>
-          <div className={s.year}>2023</div>
-          <p className={s.type}>Web app</p>
-          </div>
-          <p className={s.description}>Team project. This site will help you create your own workout program, has a variety of workouts for each body part and an explanation of how it works. I was responsible for the footer of the site and all its functionality</p>
-          <div className={s.line}></div>
-          <p className={s.skills}>Skills</p>
-          <p className={s.listskills}>HTML5 · CSS3 · JavaScript · Vite  · Communication · Teamwork </p>
-        </div>
-        
-      </li>
-      <li className={s.items}>
-      <img src={img4} alt="veggieboost" className={s.img} />
-      <div>
-          <p className={s.par_f}>Veggieboost</p>
-          <div className={s.cont_txt}>
-          <div className={s.year}>2023</div>
-          <p className={s.type}>Web app</p>
-          </div>
-          <p className={s.description}>Team project. A website that helps you easily shop for fresh vegetables from home. It is adapted for all devices, has a modal window and several animations. I developed the “organic vegetables” section, which is adapted to all devices</p>
-          <div className={s.line}></div>
-          <p className={s.skills}>Skills</p>
-          <p className={s.listskills}>HTML5 · CSS3 · JavaScript · Vite · Communication · Teamwork </p>
-        </div>
-       
-      </li>
-      <li className={s.items}>
-      <img src={img5} alt="web studio" className={s.img} />
-      <div>
-          <p className={s.par_f}>Web studio</p>
-          <div className={s.cont_txt}>
-          <div className={s.year}>2023</div>
-          <p className={s.type}>Web app</p>
-          </div>
-          <p className={s.description}>The website of the web studio is adapted for all types of devices and includes modal windows, a responsive design, effects and transitions.</p>
-          <div className={s.line}></div>
-          <p className={s.skills}>Skills</p>
-          <p className={s.listskills}>HTML5 · CSS3 · JavaScript</p>
-        </div>
-        
-      </li>
-    </ul>
+      <div className={s.pagination}>
+        {Array.from({ length: Math.ceil(projects.length / perPage) }, (_, index) => (
+          <button
+            key={index + 1}
+            className={s.pageButton}
+            onClick={() => paginate(index + 1)}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </section>
   );
 };
